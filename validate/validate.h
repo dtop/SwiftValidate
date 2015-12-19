a6f573fb7ab4ac2454a895c6d013cee7fc779595
Danilo Topalovic <danilo.topalovic@nerdsee.com>
2015-12-19 22:22:35 +0100

    Initial Commit

---
+++ validate/validate.h
@@ -0,0 +1,19 @@
+//
+//  validate.h
+//  validate
+//
+//  Created by Danilo Topalovic on 19.12.15.
+//  Copyright © 2015 Danilo Topalovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for validate.
+FOUNDATION_EXPORT double validateVersionNumber;
+
+//! Project version string for validate.
+FOUNDATION_EXPORT const unsigned char validateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <validate/PublicHeader.h>
+
+
