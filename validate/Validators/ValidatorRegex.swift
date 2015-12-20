2d215a8a14075eef251c684ddfa3e1b7771baff1
Danilo Topalovic <danilo.topalovic@nerdsee.com>
2015-12-20 15:48:51 +0100

    added regex validator

---
+++ validate/Validators/ValidatorRegex.swift
@@ -0,0 +1,74 @@
+//
+//  ValidatorRegex.swift
+//  validate
+//
+//  Created by Danilo Topalovic on 20.12.15.
+//  Copyright © 2015 Danilo Topalovic. All rights reserved.
+//
+
+import Foundation
+
+public class ValidatorRegex: BaseValidator, ValidatorProtocol {
+    
+    /// allows a nil value
+    var allowNil: Bool = false
+    
+    /// the pattern to be matched against
+    var pattern: String!
+    
+    /// expression options
+    var options: NSRegularExpressionOptions = NSRegularExpressionOptions(rawValue: 0)
+    
+    /// matching options
+    var matchingOptions: NSMatchingOptions = NSMatchingOptions(rawValue: 0)
+    
+    /// error message
+    var errorMessageValueIsNotMatching: String = NSLocalizedString("the given value is not matching to the predefined regex", comment: "ValidatorRegex - no match")
+    
+    /**
+     Easy init
+     
+     - returns: the instance
+     */
+    required public init(@noescape _ initializer: ValidatorRegex -> () = { _ in }) {
+        
+        super.init()
+        initializer(self)
+    }
+    
+    /**
+     Validates the value against a given regular expression
+     
+     - parameter value:   the value
+     - parameter context: the context (unused)
+     
+     - throws: validation errors
+     
+     - returns: true if it matches
+     */
+    public override func validate<T: Any>(value: T?, context: [String: Any?]?) throws -> Bool {
+        
+        if nil == self.pattern {
+            throw NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No pattern given for matching"])
+        }
+        
+        if self.allowNil && nil == value {
+            return true
+        }
+        
+        if let strVal = value as? String {
+        
+            let regex = try NSRegularExpression(pattern: self.pattern, options: self.options)
+            let result = regex.numberOfMatchesInString(strVal, options: self.matchingOptions, range: NSMakeRange(0, strVal.characters.count)) > 0
+            
+            if !result {
+                
+                return self.returnError(self.errorMessageValueIsNotMatching)
+            }
+            
+            return result
+        }
+        
+        throw NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Unknown or nil value type to match against"])
+    }
+}
---
+++ validateTests/ValidatorRegexTests.swift
@@ -0,0 +1,55 @@
+//
+//  ValidatorRegexTests.swift
+//  validate
+//
+//  Created by Danilo Topalovic on 20.12.15.
+//  Copyright © 2015 Danilo Topalovic. All rights reserved.
+//
+
+import XCTest
+@testable import validate
+
+class ValidatorRegexTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testValidatorCanHandleValidValue() {
+        
+        let validator = ValidatorRegex() {
+            $0.pattern = "^\\d{3}[-]\\d{2}[-]\\d{4}$"
+        }
+        
+        do {
+            
+            let result = try validator.validate("555-44-9548", context: nil)
+            XCTAssertTrue(result)
+            
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+    
+    func testValidatorCanHandleInvalidValue() {
+        
+        let validator = ValidatorRegex() {
+            $0.pattern = "^\\d{3}[-]\\d{2}[-]\\d{4}$"
+        }
+        
+        do {
+            
+            let result = try validator.validate("555-4A-9548", context: nil)
+            XCTAssertFalse(result)
+            
+        } catch _ {
+            XCTAssert(false)
+        }
+    }
+}
