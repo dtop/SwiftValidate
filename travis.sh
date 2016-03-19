8a41b3ffb8a80d310cb2e4d40158aa9126b16ced
Danilo Topalovic <danilo.topalovic@nerdsee.com>
2016-03-19 16:13:43 +0100

    travis

---
+++ travis.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+TEST_CMD="xcodebuild -project SwiftValidate.xcodeproj -scheme SwiftValidate -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6S,OS=9.1' test"
+
+which -s xcpretty
+XCPRETTY_INSTALLED=$?
+
+if [[ $TRAVIS || $XCPRETTY_INSTALLED == 0 ]]; then
+  eval "${TEST_CMD} | xcpretty"
+else
+  eval "$TEST_CMD"
+fi
